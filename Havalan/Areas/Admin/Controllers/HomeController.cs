b44a60469bf1672f6c264387d4e562509c4eceda
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-06 19:49:13 +0330

    Add HomeController

---
+++ Havalan/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Havalan.Web.Areas.Admin.Controllers;
+public class HomeController : AdminBaseController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
