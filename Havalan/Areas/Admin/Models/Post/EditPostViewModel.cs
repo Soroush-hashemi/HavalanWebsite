a1afb55e7733753f53495c2c5ce285ee61cc1af1
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-07 22:53:41 +0330

    add post view model to mvc

---
+++ Havalan/Areas/Admin/Models/Post/EditPostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Havalan.Web.Areas.Admin.Models.Post;
+public class EditPostViewModel
+{
+    public long Id { get; set; }
+    public long CategoryId { get; set; }
+    public long SubCategoryId { get; set; }
+    [Required]
+    public string Title { get; set; } = null!;
+    [Required]
+    public string Slug { get; set; } = null!;
+    [Required]
+    public string Description { get; set; } = null!;
+    [Required]
+    public IFormFile ImageFile { get; set; } = null!;
+    public int View { get; set; }
+    public bool IsFeatured { get; set; }
+    public bool IsSidebar { get; set; }
+
+}
