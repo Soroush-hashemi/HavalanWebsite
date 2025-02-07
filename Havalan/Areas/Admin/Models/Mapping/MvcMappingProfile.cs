df630b879574bf0b7f728ae5e040e56ab8fb0b26
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-07 22:53:00 +0330

    add mapping to mvc area

---
+++ Havalan/Areas/Admin/Models/Mapping/MvcMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Havalan.Application.Categories.Queries.DTOs;
+using Havalan.Web.Areas.Admin.Models.Category;
+
+namespace Havalan.Web.Areas.Admin.Models.Mapping;
+public class MvcMappingProfile : Profile
+{
+    public MvcMappingProfile()
+    {
+        CreateMap<CategoryDto, EditCategoryViewModel>();
+        CreateMap<SubCategoryDto, EditCategoryViewModel>();
+    }
+}
