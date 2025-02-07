0cb2c40da21d502f7435013967c4cbe8f18bcf85
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-07 22:57:49 +0330

    create CommentController

---
+++ Havalan/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using Havalan.Application.Comments.Commands.Delete;
+using Havalan.Application.Comments.Queries.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Havalan.Web.Areas.Admin.Controllers;
+[Route("Admin/Comment")]
+public class CommentController : AdminBaseController
+{
+    private readonly IMediator _mediator;
+    public CommentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> Index()
+    {
+        var Comments = await _mediator.Send(new GetAllCommentQuery());
+        return View(Comments);
+    }
+
+    [HttpPost("Delete")]
+    public async Task<IActionResult> Delete(long Id)
+    {
+        var result = await _mediator.Send(new DeleteCommentsCommand(Id));
+        return ResultAlert(result);
+    }
+}
