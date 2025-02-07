3b521fd707909426f359cd8f39fd1e8ac22be3b8
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-07 22:54:11 +0330

    add TrendingNews view model to mvc

---
+++ Havalan/Areas/Admin/Models/TrendingNews/TrendingNewsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Havalan.Web.Areas.Admin.Models.TrendingNews;
+public class TrendingNewsViewModel
+{
+    [Required]
+    public string Title { get; set; } = null!;
+}
