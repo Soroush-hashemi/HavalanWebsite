138c8ec25c7ddc4193125c9988949b4b1e9b7de5
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-07 22:43:39 +0330

    mapper Configurations

---
+++ Havalan/Configurations/MapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Havalan.Application.Categories.Queries.DTOs;
+using Havalan.Application.Common;
+using Havalan.Web.Areas.Admin.Models.Category;
+using Havalan.Web.Areas.Admin.Models.Mapping;
+
+namespace Havalan.Web.Configurations;
+public static class MapperConfig
+{
+    public static void ConfigureMapper(this IServiceCollection services)
+    {
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Havalan.Domain.Categories.Category, CategoryDto>();
+            cfg.CreateMap<CategoryDto, EditCategoryViewModel>();
+            cfg.AddProfile<MvcMappingProfile>();
+            cfg.AddProfile<ApplicationMappingProfile>();
+        });
+
+        IMapper mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+    }
+}
