dd36886e089bb632955a1fdd40377afe35ab92be
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-22 12:58:56 +0330

    add Id to TrendingNewsViewModel

--- Havalan/Areas/Admin/Models/TrendingNews/TrendingNewsViewModel.cs
+++ Havalan/Areas/Admin/Models/TrendingNews/TrendingNewsViewModel.cs
@@ -3,6 +3,7 @@
 namespace Havalan.Web.Areas.Admin.Models.TrendingNews;
 public class TrendingNewsViewModel
 {
+    public long Id { get; set; }
     [Required]
     public string Title { get; set; } = null!;
 }
