968270c30a1620d2bc13baa5c14c2e34566e8dfa
Soroush-hashemi <hashemisoroush85@gmail.com>
2025-02-22 16:35:38 +0330

    Create GetPostById

---
+++ Havalan.Application/Posts/Queries/GetById/GetPostByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using Havalan.Application.Posts.Queries.DTOs;
+using MediatR;
+
+namespace Havalan.Application.Posts.Queries.GetById;
+public record GetPostByIdQuery(long Id) : IRequest<PostDto>;
---
+++ Havalan.Application/Posts/Queries/GetById/GetPostByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Havalan.Application.Common.Interfaces;
+using Havalan.Application.Posts.Queries.DTOs;
+using MediatR;
+
+namespace Havalan.Application.Posts.Queries.GetById;
+public class GetPostByIdQueryHandler : IRequestHandler<GetPostByIdQuery, PostDto>
+{
+    private readonly IPostRepository _postRepository;
+    private readonly IMapper _mapper;
+    public GetPostByIdQueryHandler(IPostRepository postRepository, IMapper mapper)
+    {
+        _postRepository = postRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<PostDto> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var post = await _postRepository.GetTracking(request.Id);
+            var postDto = _mapper.Map<PostDto>(post);
+
+            return postDto;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
